077ad59ac030608730044ac20ab8929d256ea38d
GabrielRM198 <gabrielrochamagalhaes0@gmail.com>
2022-06-20 21:34:48 -0300

    desafio Funções / vai na web

---
+++ script.js
@@ -0,0 +1,34 @@
+//1 - crie uma função que exiba uma mensagem no console
+function msg(){
+    console.log("Olá, boa noite.")
+}
+
+msg()
+
+
+//2 - crie uma função que receba o seu nome como (parâmetro) e exiba no console
+function myName(nome){
+    console.log(`Bem vindo(a), ${nome}!`)
+}
+
+myName()
+
+
+//3 - crie uma função que receba nome, idade, e um estilo musical preferido (parâmetros) e exiba no console
+function about(name,age,music){
+    console.log(`Meu nome é ${name}, eu tenho${age} anos e amo ouvir ${music}`)
+}
+about("Gabriel", 23,"pagode" )
+
+//4 - crie uma função que receba um filme, uma música (parâmetros) e exiba no console
+function esportes(futebol, basquete,){
+    console.log(`Meu esporte principal é: ${futebol} e o secundário é: ${basquete}.`)
+}
+
+esportes("futebol", "basquete")
+
+
+
+
+
+
